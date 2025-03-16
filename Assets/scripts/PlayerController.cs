bc50b2e22f5de2b73b14c2aa97eb580ad219e34a
Pavlos Rorris <159206339+PavlosR@users.noreply.github.com>
2025-03-17 10:33:14 +1100

    .

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -10,8 +10,12 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rgdBody;
     [SerializeField] private float speed;
+    [SerializeField] private float Jumper = 5f;
+    [SerializeField] private float DashStrength;
 
     private Vector2 MoveForce;
+    private Vector2 JumpForce;
+    private Vector2 DashForce;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,9 +30,21 @@ public class PlayerController : MonoBehaviour
 
     public void Move(float Horizontal, float Jump) 
     {
-            MoveForce = new Vector2(Horizontal, 0.0f);
-            MoveForce = MoveForce.normalized * speed;
+            MoveForce = new Vector2(Horizontal * speed, 0f);
+            JumpForce = new Vector2(0.0f, Jump * Jumper);
+
+            // /MoveForce = MoveForce.normalized * speed;
 
             rgdBody.AddForce(MoveForce);
+            rgdBody.AddForce(JumpForce);
+
+    }
+
+    public void Dash(float Horizontal, float Vertical) 
+    {
+
+        DashForce = new Vector2(Horizontal, Vertical);
+        DashForce = DashForce.normalized * DashStrength;
+        rgdBody.AddForce(DashForce);
     }
 }
--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
 
+    private float Horizontal;
+    private float Jump;
+    private float Vertical;
+
+    private float CanDash;
+
     public PlayerController Controller;
     // Start is called before the first frame update
     void Start()
@@ -15,8 +21,16 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CanDash += Time.deltaTime;
         Horizontal = Input.GetAxisRaw("Horizontal");
+        Vertical = Input.GetAxisRaw("Vertical");
         Jump = Input.GetAxisRaw("Jump");
+
+        if(Input.GetKeyDown("q") && CanDash >= 1f) 
+        {
+            Controller.Dash(Horizontal, Vertical);
+            CanDash = 0f;
+        }
     }
 
     void FixedUpdate() 
